16137b3f4c36f7c89eaea79df8b35a919c2e2107
zencane <zenrycane@gmail.com>
2023-10-03 21:59:22 +0100

    added functiunality to read more buttons:)

---
+++ blog/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".invisible-content").hide();
+    $(document).on('click','#btn',function(){
+        var moreLessButton=$(this).closest(".w3-container").find(".invisible-content").is(":visible")?'Read More':'Read Less';
+        $(this).text(moreLessButton);
+        $(this).closest(".w3-container").find(".invisible-content").toggle();
+        $(this).closest(".w3-container").find(".visible-content").toggle();
+    });
+});
