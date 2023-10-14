606aea0d42690d61aa68938d8e126a9ff34d2f74
zencane <zenrycane@gmail.com>
2023-10-15 00:38:12 +0100

    replaced read more button with details element

--- blog/script.js
+++ blog/script.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
-    $(".invisible-content").hide();
-    $(document).on('click','#btn',function(){
-        var moreLessButton=$(this).closest(".w3-container").find(".invisible-content").is(":visible")?'Read More':'Read Less';
-        $(this).text(moreLessButton);
-        $(this).closest(".w3-container").find(".invisible-content").toggle();
-        // $(this).closest(".w3-container").find(".visible-content").toggle();
-    });
-})
+// // $(window).load(function(){
+// //     $(document).on('click',,function(){
+// //         var moreLessButton=$(this).closest(".w3-container").find(".invisible-content").is(":visible")?'Read More':'Read Less';
+// //         $(this).text(moreLessButton);
+// //         $(this).closest(".w3-container").find(".invisible-content").toggle();
+// //     });
+// // })
+
+
+// details.addEventListener("toggle", (event) => {
+//     var moreLessButton=$(this).is(":open")?'Read Masdasdasore':'Read Less';
+//     if (details.open) {
+//         $(this).closest(".summary").text(moreLessButton);
+//     } else {
+//         $(this).closest(".summary").text(moreLessButton);
+//     }
+//   });
