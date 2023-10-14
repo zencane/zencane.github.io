aaadda134054462fb2a938cb7119f45665c6c752
zencane <zenrycane@gmail.com>
2023-10-14 23:58:44 +0100

    testing details html clause

--- blog/script.js
+++ blog/script.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
         var moreLessButton=$(this).closest(".w3-container").find(".invisible-content").is(":visible")?'Read More':'Read Less';
         $(this).text(moreLessButton);
         $(this).closest(".w3-container").find(".invisible-content").toggle();
-        $(this).closest(".w3-container").find(".visible-content").toggle();
+        // $(this).closest(".w3-container").find(".visible-content").toggle();
     });
 })
